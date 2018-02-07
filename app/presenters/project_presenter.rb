241f08e7662d6ceb331c868122d2f4a11c505a9d
torreswoo <torreswoo@gmail.com>
2018-02-07 13:35:41 +0900

    Audit | add presenter for audit log

--- app/presenters/project_presenter.rb
+++ app/presenters/project_presenter.rb
@@ -1,5 +1,6 @@
 class ProjectPresenter < SimpleDelegator
   include ApplicationHelper
+  include ActionView::Helpers::DateHelper
 
   def model
     __getobj__
@@ -11,5 +12,12 @@ class ProjectPresenter < SimpleDelegator
       return '-'
     end
     "<span data-toggle='tooltip' data-html='true' title='#{model.owner.first.split('/')[1].strip}'>#{model.owner.first.split('/')[0].strip}</span>".html_safe
+  end
+
+  def audit_modified_fmt
+    modified_time = time_ago_in_words(model.updated_at)
+    modified_user = model.audits.last.nil? ? '-' : ' by ' + User.find(model.audits.last.user_id).username
+
+    modified_detail = modified_time + modified_user
   end
 end
